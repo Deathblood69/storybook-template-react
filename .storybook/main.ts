488e7fa22f9aca20c4f8c5393435badd946a5dcb
Maximus Crowe <deathblood69002@gmail.com>
2025-08-14 11:51:26 +0200

    chore(storybook): Fix accessibility

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,12 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: ["../src/components/**/*.stories.@(ts|tsx)"],
   staticDirs: ["../public"],
-  addons: ["@storybook/addon-links", "@storybook/addon-docs"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
